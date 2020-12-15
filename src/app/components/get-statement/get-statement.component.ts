8cf34fd9f802186d787a4618a446d591f482c286
nextifytechnologies <helpdesk@nextify.co.za>
2020-12-15 13:15:26 +0200

    Add loading states for Statements and funds Withdrawal

--- src/app/components/get-statement/get-statement.component.ts
+++ src/app/components/get-statement/get-statement.component.ts
@@ -4,6 +4,7 @@ import { ToastService } from './../../services/toast.service';
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { IonDatetime, ModalController } from '@ionic/angular';
 import * as superagent from 'superagent';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'app-get-statement',
@@ -14,12 +15,13 @@ export class GetStatementComponent implements OnInit, AfterViewInit {
   @ViewChild('IonStartDateInput', { static: false }) ionStartDateInput: IonDatetime;
   @ViewChild('IonEndDateInput', { static: false }) ionEndDateInput: IonDatetime;
   
-  isAllowed = true;
+  isAllowed = false;
   isLoading = false;
   constructor(
     private toast: ToastService,
     private sockets: SocketsService,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private loader: LoaderService
   ) { }
 
   ngOnInit() {}
@@ -40,6 +42,14 @@ export class GetStatementComponent implements OnInit, AfterViewInit {
   }
 
   public generateStatement(): void {
+    // Show loader when generating statement
+    this.isLoading = true;
+    this.loader.showLoader(this.isLoading);
+
+    // Disable backlight modal dismiss
+    this.modalCtrl.getTop()
+      .then((modal) => modal.backdropDismiss = false);
+    
     superagent
       .get([environment.backendServer, 'statement?token=' + this.sockets.data.token + '&start=' + this.ionStartDateInput.value + '&end=' + this.ionEndDateInput.value].join('/'))
       .end((_, response) => {
@@ -53,6 +63,14 @@ export class GetStatementComponent implements OnInit, AfterViewInit {
         } else {
           this.toast.show('ERROR: NO INTERNET CONNECTION.');
         }
+
+        // Remove loading state
+        this.isLoading = false;
+        this.loader.showLoader(this.isLoading);
+
+        // Re-enable backlight modal dismiss
+        this.modalCtrl.getTop()
+          .then((modal) => modal.backdropDismiss = true);
       });
   }
 
